80ea5a98354a515c7e54aa61cb1559725f6d37a6
roshini342 <165682446+roshini342@users.noreply.github.com>
2025-07-05 11:52:09 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
 
-export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
+export default function Todo({ task, toggleComplete, deleteTodo, editTask }) {
   return (
     <div className="Todo">
-        <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
-        <div>
-        <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={() => editTodo(task.id)} />
-        <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)} />
-        </div>
+      <p
+        onClick={() => toggleComplete(task.id)}
+        className={`todo-text ${task.completed ? 'completed' : ''}`}
+      >
+        {task.task}
+      </p>
+      <div>
+        <button onClick={() => editTask(task.id)}>✏️</button>
+        <button onClick={() => deleteTodo(task.id)}>🗑️</button>
+      </div>
     </div>
-  )
+  );
 }
